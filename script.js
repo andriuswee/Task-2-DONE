3a9a890b30bb9fd95ac481f60efd55e36ae21661
Test <Test>
2022-10-03 19:31:03 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+/* ------------------------------ TASK 2 ----------------------------
+Parašykite JS kodą, kuris skaičiuos kiek kartų buvo paspaustas mygtukas
+su tekstu "CLICK ME". Paspaudimų rezultatas turi būti matomas dešinėje
+pusėje esančiame "state" skaičiavimo bloke (<div id="btn__state">0</div>)
+------------------------------------------------------------------- */
+
+const result = 0;
+
+function clickCounter() {
+  if (typeof Storage !== "undefined") {
+    if (localStorage.clickcount) {
+      localStorage.clickcount = Number(localStorage.clickcount) + 1;
+    } else {
+      localStorage.clickcount = 1;
+    }
+    document.getElementById("result").innerHTML = "" + localStorage.clickcount;
+  }
+}
